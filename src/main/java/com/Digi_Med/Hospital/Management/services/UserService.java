522d0074ca8ce78c3ac1213e89e7fcd207bae3cf
farihaahmed82 <fariharamisha1@gmail.com>
2026-05-09 14:41:45 +0600

    Updated UserService

--- src/main/java/com/Digi_Med/Hospital/Management/services/UserService.java
+++ src/main/java/com/Digi_Med/Hospital/Management/services/UserService.java
@@ -1,7 +1,7 @@
 package com.Digi_Med.Hospital.Management.services;
 
 import com.Digi_Med.Hospital.Management.dtos.LoginRequest;
-import com.Digi_Med.Hospital.Management.dtos.LoginResponse
+import com.Digi_Med.Hospital.Management.dtos.LoginResponse;
 
 public interface UserService {
     LoginResponse login(LoginRequest loginRequest);
